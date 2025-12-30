0a6878637f3a9b870b28d5f059597ed04c4e97a3
Hariharan SP <Hariharansp2050@gmail.com>
2025-12-30 18:04:29 +0530

    changes made for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Hariharan-SP/", // TODO: Replace YOUR_REPO_NAME with your GitHub repository name
+  base: "/", // Reset to root for custom domain
 })
